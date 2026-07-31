863457f31d04562eb195c890c091109373b50874
wikitoriadata <wikitoria.data@gmail.com>
2026-07-31 15:52:26 +1200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from utils import preprocessor
+
+def run():
+    model = joblib.load(open('model.joblib','rb'))
+
+    st.title("Sentiment Analysis")
+    st.text("Basic app to detect the sentiment of text.")
+    st.text("")
+    userinput = st.text_input('Enter text below, then click the Predict button.', placeholder='Input text HERE')
+    st.text("")
+    predicted_sentiment = ""
+    if st.button("Predict"):
+        predicted_sentiment = model.predict(pd.Series('very interesting'))[0]
+        if predicted_sentiment == 1:
+            output = 'positive 👍'
+        else:
+            output = 'negative 👎'
+        sentiment=f'Predicted sentiment of "{userinput}" is {output}.'
+        st.success(sentiment)
+
+if __name__ == "__main__":
+    run()
